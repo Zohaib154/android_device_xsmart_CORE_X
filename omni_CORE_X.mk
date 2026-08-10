60d4f2b7304070401f4acf82938132ae9d36514f
Zohaib Asghar <zohaiblashari154@gmail.com>
2026-08-10 21:18:50 +0500

    Update Omni makefile to inherit TWRP configuration

--- omni_CORE_X.mk
+++ omni_CORE_X.mk
@@ -8,10 +8,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit common TWRP configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from CORE_X device
+# Inherit from CORE_X device tree
 $(call inherit-product, device/xsmart/CORE_X/device.mk)
 
 PRODUCT_DEVICE := CORE_X

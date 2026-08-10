fd1b364249f7ab62ad448dec0a7b2b8b843f539a
Zohaib Asghar <zohaiblashari154@gmail.com>
2026-08-10 21:21:02 +0500

    Remove lunch combo entries from vendorsetup.sh
    Removed lunch combo entries for omni_CORE_X.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2026 The Android Open Source Project
-
-add_lunch_combo omni_CORE_X-user
-add_lunch_combo omni_CORE_X-userdebug
-add_lunch_combo omni_CORE_X-eng
+#
+# SPDX-License-Identifier: Apache-2.0
+#

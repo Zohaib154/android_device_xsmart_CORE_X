43840a297e55464bec93f6a4f40d2a2395a9e410
Zohaib Asghar <zohaiblashari154@gmail.com>
2026-08-10 20:53:45 +0500

    Clean up in vendorsetup.sh
    Removed useless things.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,5 @@
 #
 # Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
 
 add_lunch_combo omni_CORE_X-user
 add_lunch_combo omni_CORE_X-userdebug

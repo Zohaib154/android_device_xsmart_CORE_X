fe092ea6ef0e98769434b69636091dc3adc6c41e
Zohaib Asghar <zohaiblashari154@gmail.com>
2026-08-11 09:50:46 +0500

    Add basic configuration for fox_CORE_X

---
+++ fox_CORE_X.mk
@@ -0,0 +1,8 @@
+# Inherit basic omni configuration
+$(call inherit-product, device/xsmart/CORE_X/omni_CORE_X.mk)
+
+PRODUCT_NAME := fox_CORE_X
+PRODUCT_DEVICE := CORE_X
+PRODUCT_BRAND := xsmart
+PRODUCT_MODEL := CORE_X
+PRODUCT_MANUFACTURER := xsmart
